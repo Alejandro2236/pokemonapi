b751e3d7a51565573469be0b2e25f56b8b968b1c
Santiago Bedoya Arcila <164806519+SantiagoBedoyaU@users.noreply.github.com>
2025-09-17 20:38:52 +0000

    find all pokemons

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { HttpService } from '@nestjs/axios';
+import { response } from 'express';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class PokemonService {
+
+  constructor(private readonly httpService: HttpService) {}
+
   create(createPokemonDto: CreatePokemonDto) {
     return 'This action adds a new pokemon';
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  async findAll() {
+    const { data } = await firstValueFrom(
+      this.httpService.get('https://pokeapi.co/api/v2/pokemon').pipe(
+        catchError((error: AxiosError) => {
+          console.log(error)
+          throw 'An error happened!';
+        }),
+      ),
+    );
+    return data;
   }
 
   findOne(id: number) {
